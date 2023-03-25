295ade3de148c77add0e4d202369d82862a46232
Tomas Svoboda <fredka171@gmail.com>
2023-03-25 09:00:13 +0100

    V0.1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("init test")
+from tradingview_ta import TA_Handler, Interval, Exchange
+
+
+SYMBOLS = ["BTCUSDT31H2023", "TRUUSDT", "LINKUSDT", "BANDUSDT" ]
+SCREENER = "crypto"
+EXCHANGE = "BINANCE"
+
+for i in SYMBOLS:
+    BTC = TA_Handler(
+        symbol = i,
+        screener = SCREENER,
+        exchange = EXCHANGE,
+        interval = Interval.INTERVAL_1_DAY
+        )
+
+    print(i+":", BTC.get_analysis().summary)
+
+
+
