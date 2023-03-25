f4d1fe07c8403204b8e73d3971f9346d84c049bf
Tomas Svoboda <fredka171@gmail.com>
2023-03-25 10:30:51 +0100

    V0.2

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from tradingview_ta import TA_Handler, Interval, Exchange
 
 
-SYMBOLS = ["BTCUSDT31H2023", "TRUUSDT", "LINKUSDT", "BANDUSDT" ]
+SYMBOLS = ["BTCUSDT31H2023", "TRUUSDT", "LINKUSDT", "BANDUSDT", "UNIUSD", "ENJUSDT", "SOLUSDT", "RENUSDT",
+          "LTCUSDT", "ETHUSDT"]
+
 SCREENER = "crypto"
 EXCHANGE = "BINANCE"
+inter = [Interval.INTERVAL_4_HOURS, Interval.INTERVAL_1_DAY, Interval.INTERVAL_1_WEEK]
 
 for i in SYMBOLS:
-    BTC = TA_Handler(
-        symbol = i,
-        screener = SCREENER,
-        exchange = EXCHANGE,
-        interval = Interval.INTERVAL_1_DAY
-        )
+    print("----------------------------------------------------", i, '\n')
+    for j in inter:
+        BTC = TA_Handler(
+            symbol = i,
+            screener = SCREENER,
+            exchange = EXCHANGE,
+            interval = j
+            )
 
-    print(i+":", BTC.get_analysis().summary)
+        print("--------- Interval: ", j+": -------------", '\n')
+        print(i+":", BTC.get_analysis().summary['RECOMMENDATION'])
+        print("Close price is: ", BTC.get_analysis().indicators["close"], '\n')
+   
+    
 
 
 
